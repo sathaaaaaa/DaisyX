3abd92ec78e0e3c8298ba0bca8ce93d7dc4e8687
sathaaaaaa <85862925+sathaaaaaa@users.noreply.github.com>
2021-09-07 16:58:53 +0530

    Update musicplayer.py

--- DaisyX/modules/musicplayer.py
+++ DaisyX/modules/musicplayer.py
@@ -17,47 +17,47 @@ __help__ = """
 
 <b> **Commands** </b>
 <b> **=>> Song Playing 🎧** </b>
-- /play: Play the requestd song
-- /play [yt url] : Play the given yt url
-- /play [reply yo audio]: Play replied audio
-- /splay: Play song via jio saavn
-- /ytplay: Directly play song via Youtube Music
+ /play: Play the requestd song
+ /play [yt url] : Play the given yt url
+ /play [reply yo audio]: Play replied audio
+ /splay: Play song via jio saavn
+ /ytplay: Directly play song via Youtube Music
 <b> **=>> Playback ⏯** </b>
-- /player: Open Settings menu of player
-- /skip: Skips the current track
-- /pause: Pause track
-- /resume: Resumes the paused track
-- /end: Stops media playback
-- /current: Shows the current Playing track
-- /playlist: Shows playlist
-*Player cmd and all other cmds except /play, /current  and /playlist  are only for admins of the group.
+ /player: Open Settings menu of player
+ /skip: Skips the current track
+ /pause: Pause track
+ /resume: Resumes the paused track
+ /end: Stops media playback
+ /current: Shows the current Playing track
+ /playlist: Shows playlist
+ Player cmd and all other cmds except /play, /current  and /playlist  are only for admins of the group.
 <b> **=>> Channel Music Play 🛠** </b>
 ⚪️ For linked group admins only:
-- /cplay [song name] - play song you requested
-- /csplay [song name] - play song you requested via jio saavn
-- /cplaylist - Show now playing list
-- /cccurrent - Show now playing
-- /cplayer - open music player settings panel
-- /cpause - pause song play
-- /cresume - resume song play
-- /cskip - play next song
-- /cend - stop music play
-- /userbotjoinchannel - invite assistant to your chat
+ /cplay [song name] - play song you requested
+ /csplay [song name] - play song you requested via jio saavn
+ /cplaylist - Show now playing list
+ /cccurrent - Show now playing
+ /cplayer - open music player settings panel
+ /cpause - pause song play
+ /cresume - resume song play
+ /cskip - play next song
+ /cend - stop music play
+ /userbotjoinchannel - invite assistant to your chat
 channel is also can be used instead of c ( /cplay = /channelplay )
-⚪️ If you donlt like to play in linked group:
+ ⚪️ If you donlt like to play in linked group:
 1) Get your channel ID.
 2) Create a group with tittle: Channel Music: your_channel_id
 3) Add bot as Channel admin with full perms
 4) Add @VampireLadyCountDraculahelper to the channel as an admin.
 5) Simply send commands in your group.
 <b> **=>> More tools 🧑‍🔧** </b>
-- /musicplayer [on/off]: Enable/Disable Music player
-- /admincache: Updates admin info of your group. Try if bot isn't recognize admin
-- /userbotjoin: Invite @VampireLadyCountDraculahelper Userbot to your chat
+ /musicplayer [on/off]: Enable/Disable Music player
+ /admincache: Updates admin info of your group. Try if bot isn't recognize admin
+ /userbotjoin: Invite @VampireLadyCountDraculahelper Userbot to your chat
 <b> **=>> Commands for Sudo Users ⚔️** </b>
- - /userbotleaveall - remove assistant from all chats
- - /gcast <reply to message> - globally brodcast replied message to all chats
- - /pmpermit [on/off] - enable/disable pmpermit message
-*Sudo Users can execute any command in any groups
+  /userbotleaveall - remove assistant from all chats
+  /gcast <reply to message> - globally brodcast replied message to all chats
+  /pmpermit [on/off] - enable/disable pmpermit message
+ Sudo Users can execute any command in any groups
 """
 
